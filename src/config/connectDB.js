6d90bc14c03d377cc7e011595fd08847236fb03d
Duke <agueroduc01@gmail.com>
2023-02-08 12:02:59 +0700

    railwaySequelize

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,22 +1,27 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("bookingcare", "root", "123456", {
-  // host: "localhost",
-  host: "0.0.0.0",
-  dialect: "mysql",
-  logging: false,
-});
-
-// const sequelize = new Sequelize({
-//   username: "root",
-//   password: "123456",
-//   database: "",
+// const sequelize = new Sequelize("bookingcare", "root", "123456", {
+//   // host: "localhost",
+//   host: "0.0.0.0",
 //   dialect: "mysql",
-//   port: "",
-//   host: "",
 //   logging: false,
 // });
+
+const sequelize = new Sequelize({
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
+  dialect: "mysql",
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  logging: false,
+  query: {
+    raw: true,
+  },
+  timezone: "+07:00",
+});
 
 let connection = async () => {
   try {
