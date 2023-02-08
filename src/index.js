dc0779cea511bdf12d85ec12798c65735fbe6345
Duke <agueroduc01@gmail.com>
2023-02-08 15:51:23 +0700

    configUrlReact

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json({ limit: "50mb" }));
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: process.env.URL_REACT }));
 
 app.use((req, res, next) => {
   next();
